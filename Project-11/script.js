c73163e827ed18bea7786a129c7dbea85701af17
Turana Allahverdi <129960082+Turana06@users.noreply.github.com>
2023-09-09 02:03:30 +0400

    Add files via upload

---
+++ Project-11/script.js
@@ -0,0 +1,19 @@
+// windovda pille pille gelen animation
+
+// const sections = document.querySelectorAll('section');
+
+// window.addEventListener('scroll' ,checkSections);
+// checkSections();
+// function checkSections() {
+//     const triggerBottom = window.innerHeight / 5 * 4;
+    
+//     sections.forEach((section) => {
+//         const sectionTop = section.getBoundingClientRect().top;
+
+//         if (sectionTop < triggerBottom) {
+//             section.classList.add('show');
+//         } else {
+//             section.classList.remove('show');
+//         }
+//     });
+// }
